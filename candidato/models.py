f6d57c7feb78a7f27c40bbb02cb8e41ba9d52e0d
Rafael Matthes <rafael.matthes@live.com>
2021-06-11 20:06:32 -0300

    fix: correção models candidato

--- candidato/models.py
+++ candidato/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 GENERO_CHOICE = [
@@ -22,7 +23,7 @@ class Candidato(models.Model):
     cep = models.CharField(max_length=8, default='')
     email = models.CharField(max_length=100)
     avatar = models.ImageField(blank=True)
-    # data_cadastro = models.DateTimeField(auto_now_add=True)
+    data_cadastro = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.nome
